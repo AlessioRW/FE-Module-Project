15836331c3f93b9a0cc77e2b5e03623a7523e9d5
Alessio Wright <alessiowright@outlook.com>
2023-02-14 13:45:35 +0000

    added top artists

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,8 @@ const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIREC
 export function Home(){
     const [profile, setProfile] = useState()
     const [topSongs, setTopSongs] = useState([])
+    const [topArtists, setTopArtists] = useState([])
+    const [timeRange, setTimeRange] = useState('short_term')
     const token = useContext(TokenContext)
     useEffect(() => {
         if (token){
@@ -23,7 +25,8 @@ export function Home(){
                 "Authorization": `Bearer ${token}`
               }
             fetch('https://api.spotify.com/v1/me', {headers: header}).then(res => res.json()).then(data => {setProfile(data)})
-            fetch('https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=short_term', {headers:header}).then(res => res.json()).then(data => {setTopSongs(data.items)})
+            fetch(`https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=${timeRange}`, {headers:header}).then(res => res.json()).then(data => {setTopSongs(data.items)})
+            fetch(`https://api.spotify.com/v1/me/top/artists?limit=5&time_range=${timeRange}`, {headers:header}).then(res => res.json()).then(data => {setTopArtists(data.items)})
         }
     }, [token])
 
@@ -31,7 +34,14 @@ export function Home(){
 
     if (!token){ //user is not logged in
         return(
+
+
             <div className='page-home-login'> 
+
+                <div className="info-section">
+                    <h2 className="title" >Welcome to SoundStats</h2>
+
+                </div>
                 <button onClick={() => {window.location.href=loginUrl}} className="c-btn">Login</button>
             </div>
         )
@@ -42,21 +52,48 @@ export function Home(){
                     <h1 className="username">{profile.display_name}</h1>
                     <img className="profile-picture" src={profile.images[0].url}/>
 
-                    <div className="top-songs">
-                        <h2 className="title">Top Songs</h2>
-                        {topSongs.map((song) => {
-                            const artists = []
-                            for (let artist of song.artists){
-                                artists.push(artist.name)
-                            }
-                            return (
-                                <div className="song-container">
-                                    <h2 className="song-title">{song.name}</h2>
-                                    <h3 className="artist-list">{artists.join(', ')}</h3>
-                                </div>
-                            )
-                        })}
-                    </div>
+                    <div className="top">
+                        <div className="top-songs">
+                            <h2 className="title">Top Songs</h2>
+                            {topSongs.map((song) => {
+                                //console.log(song.album.images[0].url)
+                                const artists = []
+                                for (let artist of song.artists){
+                                    artists.push(artist.name)
+                                }
+
+                                let songName = song.name
+                                if (song.name.length > 40){
+                                    songName = song.name.substring(0,40) + '...'
+                                }
+                        
+                                return (
+                                    <div className="song-container">
+                                        <img className="album-cover" src={song.album.images[0].url}/>
+                                        <h2 className="song-title">{songName}</h2>
+                                        <h3 className="artist-list"> by {artists.join(', ')}</h3>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        
+                        <div className="top-artists">
+                            <h2 className="title"> Top Artists</h2>
+                            <div className="artists">
+                                {topArtists.map((artist) => {
+
+                                    return (
+                                        <div className="artist-container">
+                                            <h2 className="name">{artist.name}</h2>
+                                            <img className="image" src={artist.images[0].url}/>
+                                                
+                                        </div>
+                                    )
+                                })}
+                            </div>
+
+                        </div>
+                    </div>                    
                 </div>
             )
         }
